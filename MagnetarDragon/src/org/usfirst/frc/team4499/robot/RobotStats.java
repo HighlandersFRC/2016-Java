ca316f8582566c1eb3343c95b531580a3e1e6a94
Tyler Harmon <tyler.harmon44@gmail.com>
2016-09-05 14:59:39 -0600

    Added basic drive support

---
+++ MagnetarDragon/src/org/usfirst/frc/team4499/robot/RobotStats.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team4499.robot;
+
+public class RobotStats {
+	
+	public static double robotWeight = 70; // robot weight in lb's
+	public static double driveDiameter = 8; // robot drive wheel diameters in inches
+	public static double driveWheelFriction = 0; // coeffecient of friction on drive wheels
+	public static double floorFriction = 0;
+	public static double referenceVoltage = 12;
+	
+	
+	
+
+}
